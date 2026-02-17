a179b4e5c7f2d62d05df6dc981a1ad76f38133a9
TheePoseidon <j.ngugigiat@alustudent.com>
2026-02-17 11:52:04 +0200

    Select your fruit of choice

---
+++ simple_menu/menu.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	     int choice;
+	         char *fruits[] = {"Apple", "Banana", "Mango"};
+		     for (;;)  // 
+			       //     {
+			       //             printf("\n1. Apple\n2. Banana\n3. Mango\n4. Exit\n");
+			       //                     printf("Choose your favorite fruit: ");
+			       //                             scanf("%d", &choice);
+			       //
+			       //                             	 if (choice == 4) break;       
+			       //                             	         if (choice < 1 || choice > 4) continue;
+			       //
+			       //                             	         	 printf("You chose: %s\n", fruits[choice-1]);
+			       //                             	         	       }
+			       //                             	         	            printf("Goodbye!\n");
+			       //                             	         	                return 0;
+			       //                             	         	                }
+			       //
