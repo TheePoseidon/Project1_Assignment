edfaaba470c074211ce6b35815c510bf5927a9a7
TheePoseidon <j.ngugigiat@alustudent.com>
2026-06-07 15:56:46 +0200

    Calculates and prints formatted output

---
+++ calculation/average.c
@@ -0,0 +1,13 @@
+int main () {
+	float num1 = 20.0;
+	float num2 = 45.0;
+	float num3 = 89.0;
+
+	float average = (num1 + num2 + num3) / 3;
+
+	printf("1st number: %.2f\n", num1);
+	printf("2nd number: %.2f\n", num2);
+	printf("3rd number: %.2f\n", num3);
+
+	return 0;
+}
